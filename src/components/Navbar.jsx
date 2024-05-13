1704b80c8dc556762e4fca2c84bcd24fd8e293b1
HRISHIKESH R <mrhrishikesh.r@gmail.com>
2024-05-13 12:49:18 +0530

    test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
         >
           <img src={Rishi} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
-            RISHI &nbsp;| <span className="sm:block hidden">RISHI VOX</span>
+            RISHI &nbsp;| <span className="sm:block hidden"> RISHI VOX</span>
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
