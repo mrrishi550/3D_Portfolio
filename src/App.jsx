304f4a2878dea465c717daebe78960f38f3841ce
HRISHIKESH R <mrhrishikesh.r@gmail.com>
2024-05-13 12:41:22 +0530

    deplooy

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+} from "./components";
+import StarsCanvas from "./components/canvas/Stars";
+const App = () => {
+  return (
+    <div className="relative z-0 bg-primary ">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar />
+        <Hero />
+      </div>
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <Feedbacks />
+      <div className="relative z-0">
+        <Contact />
+        <StarsCanvas />
+      </div>
+    </div>
+  );
+};
+export default App;
---
+++ src/components/canvas/index.js
@@ -0,0 +1,6 @@
+import EarthCanvas from "./Earth";
+import BallCanvas from "./Ball";
+import ComputersCanvas from "./Computers";
+import Stars from "./Stars";
+
+export { EarthCanvas, BallCanvas, ComputersCanvas, Stars };
