c1fd1e279216cd6c2635eb86215c500bdff29f45
HRISHIKESH R <mrhrishikesh.r@gmail.com>
2024-06-07 11:40:18 +0530

    maintainance 1

--- src/App.jsx
+++ src/App.jsx
@@ -17,13 +17,13 @@ const App = () => {
         <Hero />
       </div>
       <About />
-      <Experience />
+      {/* <Experience /> */}
       <Tech />
       {/* <Works /> */}
       {/* <Feedbacks /> */}
       <div className="relative z-0">
         <Contact />
-        <StarsCanvas />
+        <StarsCanvas /> 
       </div>
     </div>
   );
